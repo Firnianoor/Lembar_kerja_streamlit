d080770e208e0b8face08830744cfb2637a7746d
firnianoor <firnianoorkuliah@gmail.com>
2025-05-04 23:15:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+import matplotlib.pyplot as plt
 import altair as alt
 import plotly.express as px
 from datetime import date, time
